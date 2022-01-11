967f2432a588d2d73654f8e2d8f193973d77b395
f3lix1110 <felixhdz1110@gmail.com>
2022-01-11 17:09:00 -0600

    Actividad de equipo

---
+++ EjerciciosGithub.js
@@ -0,0 +1,194 @@
+//Part 1
+var x = false;
+console.log(x);
+
+var y = Number(x);
+console.log(typeof(y));
+
+var z = String(x)
+console.log(typeof(z));
+
+console.log(typeof(x));
+
+
+console.log("------------------------");
+var x = true;
+console.log(x);
+var y = Number(x)
+console.log(typeof(y));
+
+var z = String(x)
+console.log(typeof(z))
+
+console.log(typeof(x));
+
+console.log("------------------------");
+var x = 0;
+console.log(x);
+console.log(typeof(x));
+
+var z = String(x)
+console.log(typeof(z));
+
+var x = !!"false";
+console.log(typeof(x));
+
+console.log("------------------------");
+var x = 1;
+console.log(x);
+console.log(typeof(x));
+
+var z = String(x)
+console.log(typeof(z));
+
+var x = !!"false";
+console.log(typeof(x));
+
+console.log("------------------------");
+var x = "0";
+console.log(x);
+
+var y = Number(x);
+console.log(typeof(y));
+
+var z = String(x)
+console.log(typeof(z));
+
+var x = !!"false";
+console.log(typeof(x));
+
+console.log("------------------------");
+var x = "000";
+console.log(x);
+
+var y = Number(x);
+console.log(typeof(y));
+
+var z = String(x)
+
+console.log(typeof(z));
+
+var x = !!"false";
+console.log(typeof(x));
+
+console.log("------------------------");
+var x = "1";
+console.log(x);
+
+var y = Number(x);
+console.log(typeof(y));
+
+var z = String(x)
+console.log(typeof(z));
+
+var x = !!"false";
+console.log(typeof(x));
+
+console.log("------------------------");
+var x = NaN;
+console.log(x);
+
+var y = Number(x);
+console.log(typeof(y));
+
+var z = String(x)
+console.log(typeof(z));
+
+var x = !!"false";
+console.log(typeof(x));
+
+console.log("------------------------");
+var x = Infinity;
+console.log(x);
+
+var y = Number(x);
+console.log(typeof(y));
+
+var z = String(x)
+console.log(typeof(z));
+
+var x = !!"false";
+console.log(typeof(x));
+
+console.log("------------------------");
+var x = -Infinity;
+console.log(x);
+
+var y = Number(x);
+console.log(typeof(y));
+
+var z = String(x)
+console.log(typeof(z));
+
+var x = !!"false";
+console.log(typeof(x));
+
+console.log("------------------------");
+var x = "";
+console.log(x);
+
+var y = Number(x);
+console.log(typeof(y));
+
+var z = String(x)
+console.log(typeof(z));
+
+var x = !!"false";
+console.log(typeof(x));
+
+console.log("------------------------");
+var x = "20";
+console.log(x);
+
+var y = Number(x);
+console.log(typeof(y));
+
+var z = String(x)
+console.log(typeof(z));
+
+var x = !!"false";
+console.log(typeof(x));
+
+console.log("------------------------");
+var x = null;
+console.log(x);
+
+var y = Number(x);
+console.log(typeof(y));
+
+var z = String(x)
+console.log(typeof(z));
+
+var x = !!"false";
+console.log(typeof(x));
+
+console.log("------------------------");
+var x = undefined;
+console.log(x);
+
+var y = Number(x);
+console.log(typeof(y));
+
+var z = String(x)
+console.log(typeof(z));
+
+var x = !!"false";
+console.log(typeof(x));
+
+
+console.log("------------------------");
+//Part 2 Calculator
+
+var birthYear = 1998;
+var futureYear  = 2022;
+var age  = futureYear - birthYear;
+console.log('I will be either ' + age + ' or ' + (age - 1));
+
+console.log("------------------------");
+// Information Card
+
+var nombre = "Felix";
+var loca = "Gdl";
+var hobby = "See Movies";
+
+console.log("Hi, my name is "+ nombre +". I live in "+loca+" and enjoy "+hobby);
